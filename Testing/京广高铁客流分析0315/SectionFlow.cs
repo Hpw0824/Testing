4e3d88d3f4beecc624e9156458c15319dec6ce4a
Hpw0824 <387757936@qq.com>
2021-07-01 14:54:34 +0800

    我的第一次提交代码

---
+++ Testing/京广高铁客流分析0315/SectionFlow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 京广高铁客流分析0315
+{
+    public class SectionFlow
+    {
+        public StationInfo StartStation;
+
+        public StationInfo EndStation;
+
+        public string StartTime;
+
+        public string EndTime;
+
+        public Int16 Num;
+    }
+}
